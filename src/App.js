e500e6bcefc5a3ec4b514c5ccbae963003ea43d8
Abhishek Sonawane <abhishek@gmail.com>
2023-07-25 19:47:58 +0530

    save all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Facilities from './pages/Facilities';
+import Home from './pages/Home';
+import Contactus from './pages/Contactus';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Rooms from './pages/Rooms';
+import Roomdetail from './pages/Roomdetail';
+import Roomsdet from './pages/Roomsdet';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div>
+    //   <Home/>
+    //   {/* <About/> */}
+    //   {/* <Facilities/> */}
+    // </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/rooms" element={<Rooms/>}/>
+        <Route path="/facilities" element={<Facilities/>}/>
+        <Route path="/contact-us" element={<Contactus/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/room-detail" element={<Roomdetail/>}/>
+        <Route path="/room-det" element={<Roomsdet/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
